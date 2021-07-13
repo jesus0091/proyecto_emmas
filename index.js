944b188caa9a3211459034eab62246f77af889d7
JesusHernandez <jesushernandez120491@gmail.com>
2021-07-13 17:38:18 -0300

    Proyecto EMMAS

--- index.js
+++ index.js
@@ -3,3 +3,78 @@ window.onload = router();
 $(window).on("hashchange", function () {
   router();
 });
+
+popupWhatsApp = () => {
+  
+  let btnClosePopup = document.querySelector('.closePopup');
+  let btnOpenPopup = document.querySelector('.whatsapp-button');
+  let popup = document.querySelector('.popup-whatsapp');
+  let sendBtn = document.getElementById('send-btn');
+
+  btnClosePopup.addEventListener("click",  () => {
+    popup.classList.toggle('is-active-whatsapp-popup')
+  })
+  
+  btnOpenPopup.addEventListener("click",  () => {
+    popup.classList.toggle('is-active-whatsapp-popup')
+     popup.style.animation = "fadeIn .6s 0.0s both";
+  })
+  
+  sendBtn.addEventListener("click", () => {
+  let msg = document.getElementById('whats-in').value;
+  let relmsg = msg.replace(/ /g,"%20");
+    //just change the numbers "1515551234567" for your number. Don't use +001-(555)1234567     
+   window.open('https://wa.me/+5491164212419?text='+relmsg, '_blank'); 
+  
+  });
+
+  setTimeout(() => {
+    popup.classList.toggle('is-active-whatsapp-popup');
+  }, 3000);
+}
+
+popupWhatsApp();
+
+/*--------------------------------------------------------------------------------------------------------------*/ 
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="mascota">nuestros Brownies</i>',
+		'<i class="mascota">nuestras Chocotejas</i>',
+		'<i class="mascota">nuestros Alfajores</i>',
+		'<i class="mascota">...</i>'
+	],
+
+	//stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+	shuffle: false, // Alterar el orden en el que escribe las palabras.
+	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	loop: true, // Repetir el array de strings
+	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+	showCursor: true, // Mostrar cursor palpitanto
+	cursorChar: '|', // Caracter para el cursor
+	contentType: 'html', // 'html' o 'null' para texto sin formato
+});
+
+// var typed = new Typed('.typed', {
+// 	strings: [
+// 		'<i class="mascota">Gato</i>',
+// 		'<i class="mascota">Perro</i>',
+// 		'<i class="mascota">Conejo</i>',
+// 		'<i class="mascota">Pez</i>',
+// 	],
+// 	stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+// 	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+// 	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+// 	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+// 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+// 	shuffle: false, // Alterar el orden en el que escribe las palabras.
+// 	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+// 	loop: true, // Repetir el array de strings
+// 	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+// 	showCursor: true, // Mostrar cursor palpitanto
+// 	cursorChar: '|', // Caracter para el cursor
+// 	contentType: 'html', // 'html' o 'null' para texto sin formato
+// });
